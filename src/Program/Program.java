f9abdbe3abc3341fb51c5afa638de61c4c7b0eaa
Victor Assis <victormassis@gmail.com>
2023-02-21 20:28:12 -0300

    Refactored some classes

---
+++ src/Program/Program.java
@@ -0,0 +1,19 @@
+package Program;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		System.out.println("Portal Cursos Online!");
+		System.out.println();
+		System.out.println("Informe o seu nome e seu ID para se registrar!");
+		System.out.println("Nome: ");
+	}
+
+}
--- src/objects/Alumni.java
+++ src/objects/Alumni.java
@@ -1,5 +1,52 @@
 package objects;
 
+import java.util.Objects;
+
 public class Alumni {
+	private String name;
+	private Integer id;
+	
+	public Alumni() {
+	}
+
+	public Alumni(String name, Integer id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alumni other = (Alumni) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 
 }
